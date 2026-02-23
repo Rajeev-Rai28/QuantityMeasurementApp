e25124be5140ba85b83ecb47539545780e1ce761
Rajeev Rai <rr6921727@gmai.com>
2026-02-23 15:10:03 +0530

    [Rajeev] : Add QuantityMeasurementApp.java

--- src/main/java/com/quantity/app/QuantityMeasurementApp.java
+++ src/main/java/com/quantity/app/QuantityMeasurementApp.java
@@ -1,13 +1,30 @@
 package com.quantity.app;
 
-import com.quantity.domain.length.Feet;
+import com.quantity.domain.length.LengthUnit;
+import com.quantity.domain.length.Quantity;
 
 public class QuantityMeasurementApp {
 
-    public static void main(String[] args) {
-        Feet first = new Feet(1.0);
-        Feet second = new Feet(1.0);
+    public static boolean demonstrateLengthEquality(Quantity q1, Quantity q2) {
+        return q1.equals(q2);
+    }
 
-        System.out.println("Are equal? " + first.equals(second));
+    public static void demonstrate(String label, Quantity q1, Quantity q2) {
+        System.out.println(label + " : " + demonstrateLengthEquality(q1, q2));
+    }
+
+    public static void main(String[] args) {
+
+        demonstrate("Feet == Feet",
+                new Quantity(1.0, LengthUnit.FEET),
+                new Quantity(1.0, LengthUnit.FEET));
+
+        demonstrate("Inches == Inches",
+                new Quantity(1.0, LengthUnit.INCH),
+                new Quantity(1.0, LengthUnit.INCH));
+
+        demonstrate("Feet == Inches",
+                new Quantity(1.0, LengthUnit.FEET),
+                new Quantity(12.0, LengthUnit.INCH));
     }
 }
