e4372ac07983f24b06b000585bdbd490aeeafc98
Rajeev Rai <rr6921727@gmai.com>
2026-02-23 15:56:35 +0530

    [Rajeev] : Add Length.java

--- src/main/java/com/quantity/domain/length/Length.java
+++ src/main/java/com/quantity/domain/length/Length.java
@@ -1,39 +1,52 @@
 package com.quantity.domain.length;
 
+
 import java.util.Objects;
 
-public class Quantity {
-
-    private static final double EPSILON = 0.0001;
+public final class Length {
 
     private final double value;
     private final LengthUnit unit;
 
-    public Quantity(double value, LengthUnit unit) {
-        if(unit == null) {
+    public Length(double value, LengthUnit unit) {
+        if (unit == null) {
             throw new IllegalArgumentException("Unit cannot be null");
         }
         this.value = value;
         this.unit = unit;
     }
 
-    private double toBaseUnit() {
-        return unit.toBaseUnit(value);
+    public double getValue() {
+        return value;
+    }
+
+    public LengthUnit getUnit() {
+        return unit;
+    }
+
+    double toBase() {
+        return unit.toBase(value);
     }
 
     @Override
-    public boolean equals(Object obj) {
-
-        if(this == obj) return true;
-        if(obj == null || getClass() != obj.getClass()) return false;
-
-        Quantity other = (Quantity) obj;
-
-        return Math.abs(this.toBaseUnit() - other.toBaseUnit()) < EPSILON;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Length)) return false;
+        Length other = (Length) o;
+        return Double.compare(round(this.toBase()), round(other.toBase())) == 0;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(Math.round(toBaseUnit() / EPSILON));
+        return Objects.hash(round(toBase()));
+    }
+
+    private double round(double val) {
+        return Math.round(val * 100.0) / 100.0;
+    }
+
+    @Override
+    public String toString() {
+        return value + " " + unit;
     }
 }
