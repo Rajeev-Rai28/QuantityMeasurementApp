7aaaa3c628212b4607c71fa139e104ca9e34857f
Rajeev Rai <rr6921727@gmai.com>
2026-03-17 11:32:27 +0530

    [Rajeev] : Added NTierArchitectureRefactor feature

--- src/main/java/com/quantity/unit/VolumeUnit.java
+++ src/main/java/com/quantity/unit/VolumeUnit.java
@@ -1,50 +1,35 @@
 package com.quantity.unit;
 
-import com.quantity.unit.IMeasurable;
-
+//volume units
 public enum VolumeUnit implements IMeasurable {
 
-    LITRE(1.0, "Litre"),
-    MILLILITRE(0.001, "Millilitre"),
-    GALLON(3.78541, "Gallon");
+ LITRE(1.0),
+ MILLILITRE(0.001),
+ GALLON(3.78541);
 
-    private final double conversionFactor;
-    private final String unitName;
+ private final double conversionFactor;
 
-    VolumeUnit(double conversionFactor, String unitName) {
-        this.conversionFactor = conversionFactor;
-        this.unitName = unitName;
-    }
+ VolumeUnit(double conversionFactor) {
+     this.conversionFactor = conversionFactor;
+ }
 
-    @Override
-    public double getConversionFactor() {
-        return conversionFactor;
-    }
+ @Override
+ public double getConversionFactor() {
+     return conversionFactor;
+ }
 
-    @Override
-    public double convertToBaseUnit(double value) {
-        return value * conversionFactor;
-    }
+ @Override
+ public double convertToBaseUnit(double value) {
+     return value * conversionFactor; // convert to litre
+ }
 
-    @Override
-    public double convertFromBaseUnit(double baseValue) {
-        return baseValue / conversionFactor;
-    }
+ @Override
+ public double convertFromBaseUnit(double baseValue) {
+     return baseValue / conversionFactor; // from litre
+ }
 
-    @Override
-    public String getUnitName() {
-        return unitName;
-    }
-
-	@Override
-	public double toBaseUnit(double value) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double fromBaseUnit(double baseValue) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+ @Override
+ public String getUnitName() {
+     return this.name();
+ }
 }
